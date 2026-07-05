da5a43f6393461fb0b3ec2a28f82b929c0be74e8
hugobrunet2014-svg <hugo.brunet2014@gmail.com>
2026-07-06 00:14:10 +0200

    Update SkinListener.java

--- src/main/java/fr/hugo/identitystealer/SkinListener.java
+++ src/main/java/fr/hugo/identitystealer/SkinListener.java
@@ -7,7 +7,7 @@ import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.inventory.InventoryClickEven;
 import org.bukkit.event.inventory.InventoryType;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.ItemStack;
