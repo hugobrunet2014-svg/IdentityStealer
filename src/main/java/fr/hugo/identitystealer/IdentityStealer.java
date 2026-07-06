e1f81e42bc503e49cbd499f12f667f0a2f2cf2c6
hugobrunet2014-svg <hugo.brunet2014@gmail.com>
2026-07-06 11:21:08 +0200

    Update IdentityStealer.java

--- src/main/java/fr/hugo/identitystealer/IdentityStealer.java
+++ src/main/java/fr/hugo/identitystealer/IdentityStealer.java
@@ -1,1 +1,24 @@
 
+package fr.hugo.identitystealer;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class IdentityStealer extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Enregistrement de tous nos détecteurs
+        getServer().getPluginManager().registerEvents(new PlayerKillListener(), this);
+        getServer().getPluginManager().registerEvents(new SkinListener(this), this);
+        getServer().getPluginManager().registerEvents(new IdentityListener(this), this);
+        
+        getLogger().info("==========================================");
+        getLogger().info("IdentityStealer v1.0 par Hugo - ACTIVE");
+        getLogger().info("==========================================");
+    }
+
+    @Override
+    public void onDisable() {
+        getLogger().info("IdentityStealer v1.0 - DESACTIVE");
+    }
+}
