3c5ba6734f6f8db25d95ac47255a6547df00a01d
hugobrunet2014-svg <hugo.brunet2014@gmail.com>
2026-07-06 14:12:13 +0200

    Update IdentityStealer.java

--- src/main/java/fr/hugo/identitystealer/IdentityStealer.java
+++ src/main/java/fr/hugo/identitystealer/IdentityStealer.java
@@ -6,10 +6,11 @@ public class IdentityStealer extends JavaPlugin {
 
     @Override
     public void onEnable() {
+        // Uniquement notre listener principal actif
         getServer().getPluginManager().registerEvents(new IdentityListener(this), this);
 
         getLogger().info("=======================================");
-        getLogger().info("IdentityStealer v1.0 - CODES ORIGINAUX REPRIS");
+        getLogger().info("IdentityStealer v1.0 - INITIALISE");
         getLogger().info("=======================================");
     }
 
