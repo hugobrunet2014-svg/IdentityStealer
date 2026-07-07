140b4d7196aecbb175388148f2ab3c4c365a72b7
hugobrunet2014-svg <hugo.brunet2014@gmail.com>
2026-07-07 12:18:56 +0200

    Update IdentityListener.java

--- src/main/java/fr/hugo/identitystealer/IdentityListener.java
+++ src/main/java/fr/hugo/identitystealer/IdentityListener.java
@@ -1,16 +1,28 @@
 package fr.hugo.identitystealer;
 
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
-import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.AsyncPlayerChatEvent;
+import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.SkullMeta;
 
 public class IdentityListener implements Listener {
 
-    @EventHandler(priority = EventPriority.HIGHEST)
-    public void onPlayerChat(AsyncPlayerChatEvent event) {
-        // Cette méthode simple remplace le format du chat
-        // On force l'affichage du nom comme tu le souhaites
-        event.setFormat("%s: %s"); 
+    @EventHandler
+    public void onPlayerDeath(PlayerDeathEvent event) {
+        Player victim = event.getEntity();
+        
+        // Créer l'item Tête
+        ItemStack head = new ItemStack(Material.PLAYER_HEAD);
+        SkullMeta meta = (SkullMeta) head.getItemMeta();
+        
+        // Assigner le propriétaire de la tête
+        meta.setOwningPlayer(victim);
+        head.setItemMeta(meta);
+        
+        // Ajouter la tête au loot de la mort
+        event.getDrops().add(head);
     }
 }
