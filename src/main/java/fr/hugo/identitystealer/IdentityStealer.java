4174687c6ace9aac555d5a3b1a2a77de2bafb16e
hugobrunet2014-svg <hugo.brunet2014@gmail.com>
2026-07-07 12:05:34 +0200

    Update IdentityStealer.java

--- src/main/java/fr/hugo/identitystealer/IdentityStealer.java
+++ src/main/java/fr/hugo/identitystealer/IdentityStealer.java
@@ -1,22 +1,40 @@
 package fr.hugo.identitystealer;
 
-import com.comphenix.protocol.ProtocolLibrary;
-import com.comphenix.protocol.ProtocolManager;
-import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
-public class IdentityStealer extends JavaPlugin {
-    
-    private static ProtocolManager protocolManager;
+public class StealCommand implements CommandExecutor {
 
     @Override
-    public void onEnable() {
-        protocolManager = ProtocolLibrary.getProtocolManager();
-        // Enregistrement de la commande
-        getCommand("steal").setExecutor(new StealCommand());
-        getLogger().info("IdentityStealer activé avec ProtocolLib !");
-    }
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        if (!(sender instanceof Player)) return true;
+        Player player = (Player) sender;
 
-    public static ProtocolManager getProtocolManager() {
-        return protocolManager;
+        if (args.length < 1) {
+            player.sendMessage("§cUsage: /steal <pseudo>");
+            return true;
+        }
+
+        String targetName = args[0];
+
+        // 1. Changement du skin via SkinsRestorer
+        player.performCommand("skinsrestorer set " + targetName);
+
+        // 2. Rafraîchissement visuel pour que les autres voient le changement
+        for (Player online : player.getServer().getOnlinePlayers()) {
+            if (online != player) {
+                online.hidePlayer(player);
+                // Petit délai pour laisser le temps au serveur de traiter le changement
+                player.getServer().getScheduler().runTaskLater(
+                    org.bukkit.plugin.java.JavaPlugin.getPlugin(IdentityStealer.class), 
+                    () -> online.showPlayer(player), 20L
+                );
+            }
+        }
+
+        player.sendMessage("§aIdentité de " + targetName + " volée avec succès !");
+        return true;
     }
 }
