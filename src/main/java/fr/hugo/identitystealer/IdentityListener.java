69e107e77ef65cfb90dc78963b1e66716ba38720
hugobrunet2014-svg <hugo.brunet2014@gmail.com>
2026-07-07 13:42:05 +0200

    Update IdentityListener.java

--- src/main/java/fr/hugo/identitystealer/IdentityListener.java
+++ src/main/java/fr/hugo/identitystealer/IdentityListener.java
@@ -1,6 +1,5 @@
 package fr.hugo.identitystealer;
 
-import net.skinsrestorer.api.SkinsRestorerProvider;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -14,19 +13,10 @@ public class IdentityListener implements Listener {
     @EventHandler
     public void onPlayerDeath(PlayerDeathEvent event) {
         Player victim = event.getEntity();
-        
         ItemStack head = new ItemStack(Material.PLAYER_HEAD);
         SkullMeta meta = (SkullMeta) head.getItemMeta();
-        
-        // AU LIEU DE JUSTE LE NOM, ON ESSAYE DE RÉCUPÉRER LA SIGNATURE DU SKIN
-        try {
-            // Cela demande à SkinsRestorer la vraie texture du joueur
-            meta.setOwningPlayer(victim);
-            head.setItemMeta(meta);
-        } catch (Exception e) {
-            // Si ça échoue, on met au moins le nom pour éviter que le plugin crash
-        }
-        
+        meta.setOwningPlayer(victim);
+        head.setItemMeta(meta);
         event.getDrops().add(head);
     }
 }
