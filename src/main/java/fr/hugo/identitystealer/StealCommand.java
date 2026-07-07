6e6e928bdd9cf21384b2f726af9732ddda4ee591
hugobrunet2014-svg <hugo.brunet2014@gmail.com>
2026-07-07 11:27:15 +0200

    Update StealCommand.java

--- src/main/java/fr/hugo/identitystealer/StealCommand.java
+++ src/main/java/fr/hugo/identitystealer/StealCommand.java
@@ -20,20 +20,24 @@ public class StealCommand implements CommandExecutor {
 
         String targetName = args[0];
 
-        // 1. Enregistrer le vol dans la mémoire du plugin
+        // 1. Enregistrer le vol pour le chat
         IdentityListener.stolenIdentities.put(player.getUniqueId(), targetName);
 
-        // 2. Donner la tête
+        // 2. Commande pour changer le skin (ex: via le plugin SkinsRestorer ou autre)
+        // Remplace "skin set" par la commande exacte de ton plugin de skin
+        Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "skin set " + targetName);
+
+        // 3. Commande pour le nom (NametagEditX)
+        Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "ne clear " + player.getName());
+        Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "ne " + player.getName() + " " + targetName);
+
+        // 4. Donner la tête
         ItemStack head = new ItemStack(Material.PLAYER_HEAD);
         SkullMeta meta = (SkullMeta) head.getItemMeta();
         PlayerProfile profile = Bukkit.createPlayerProfile(targetName);
         meta.setOwnerProfile(profile);
         head.setItemMeta(meta);
         player.getInventory().addItem(head);
-
-        // 3. Appliquer le nom (NametagEditX)
-        Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "ne clear " + player.getName());
-        Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "ne " + player.getName() + " " + targetName);
 
         player.sendMessage("§aTu as pris l'identité de " + targetName);
         return true;
