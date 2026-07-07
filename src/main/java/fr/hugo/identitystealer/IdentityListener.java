6d07b5e3dbc3fa43f5a29f1ce30bd4424f350d86
hugobrunet2014-svg <hugo.brunet2014@gmail.com>
2026-07-07 11:32:11 +0200

    Update IdentityListener.java

--- src/main/java/fr/hugo/identitystealer/IdentityListener.java
+++ src/main/java/fr/hugo/identitystealer/IdentityListener.java
@@ -1,33 +1,16 @@
 package fr.hugo.identitystealer;
 
-import io.papermc.paper.event.player.AsyncChatEvent;
-import net.kyori.adventure.text.Component;
-import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
-
-import java.util.HashMap;
-import java.util.Map;
-import java.util.UUID;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
 
 public class IdentityListener implements Listener {
 
-    // On stocke qui a volé l'identité de qui (UUID du voleur -> Nom de la victime)
-    public static Map<UUID, String> stolenIdentities = new HashMap<>();
-
     @EventHandler(priority = EventPriority.HIGHEST)
-    public void onChat(AsyncChatEvent event) {
-        Player player = event.getPlayer();
-
-        // Si le joueur a volé une identité
-        if (stolenIdentities.containsKey(player.getUniqueId())) {
-            String victimName = stolenIdentities.get(player.getUniqueId());
-
-            // On change le nom affiché dans le chat
-            event.renderer((source, sourceDisplayName, message, viewer) -> 
-                Component.text(victimName + ": ").append(message)
-            );
-        }
+    public void onPlayerChat(AsyncPlayerChatEvent event) {
+        // Cette méthode simple remplace le format du chat
+        // On force l'affichage du nom comme tu le souhaites
+        event.setFormat("%s: %s"); 
     }
 }
