180e8d23219a00892d5fa92f38eab0720ef4bc3b
hugobrunet2014-svg <hugo.brunet2014@gmail.com>
2026-07-07 12:09:27 +0200

    Update IdentityStealer.java

--- src/main/java/fr/hugo/identitystealer/IdentityStealer.java
+++ src/main/java/fr/hugo/identitystealer/IdentityStealer.java
@@ -1,40 +1,15 @@
 package fr.hugo.identitystealer;
 
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandExecutor;
-import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
 
-public class StealCommand implements CommandExecutor {
+public class IdentityStealer extends JavaPlugin {
 
     @Override
-    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-        if (!(sender instanceof Player)) return true;
-        Player player = (Player) sender;
-
-        if (args.length < 1) {
-            player.sendMessage("§cUsage: /steal <pseudo>");
-            return true;
+    public void onEnable() {
+        // Enregistrement de la commande
+        if (getCommand("steal") != null) {
+            getCommand("steal").setExecutor(new StealCommand());
         }
-
-        String targetName = args[0];
-
-        // 1. Changement du skin via SkinsRestorer
-        player.performCommand("skinsrestorer set " + targetName);
-
-        // 2. Rafraîchissement visuel pour que les autres voient le changement
-        for (Player online : player.getServer().getOnlinePlayers()) {
-            if (online != player) {
-                online.hidePlayer(player);
-                // Petit délai pour laisser le temps au serveur de traiter le changement
-                player.getServer().getScheduler().runTaskLater(
-                    org.bukkit.plugin.java.JavaPlugin.getPlugin(IdentityStealer.class), 
-                    () -> online.showPlayer(player), 20L
-                );
-            }
-        }
-
-        player.sendMessage("§aIdentité de " + targetName + " volée avec succès !");
-        return true;
+        getLogger().info("IdentityStealer a bien démarré !");
     }
 }
